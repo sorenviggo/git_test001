5f30eb71059b052c12198a66405eb8983579da40
P1940176750 <sorenviggo@hotmail.com>
2018-09-18 09:27:36 +0000

    Initial

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("dk.sorenviggo.git_test001.Git_Test001.controller.View1", {
+
+	});
+});
